3acfe1e6d8376dac9d945d9546d4132b68b1a9ec
Dixie Flatline <umi0451@gmail.com>
2021-12-15 21:30:18 +0200

    Initial setup.

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+import re, subprocess
+versions = sorted([
+		m.group(1).split('.')
+		for m in
+		(re.match(r'^.*(\d+[.]\d+[.]\d+)$', line) for line in subprocess.check_output(['git', 'tag']).decode().splitlines())
+		if m
+		])
+version = '.'.join(versions[-1]) if versions else '0.0.0'
+setuptools.setup(
+		name='blackcompany',
+		version=version,
+		packages=['blackcompany'],
+		install_requires=[
+			'bottle',
+			],
+		)
