a1be58ca20a6365c57d152a0cc0a2ec34d630bcc
Dixie Flatline <umi0451@gmail.com>
2021-12-16 21:28:20 +0200

    Option to initialize concurrency for web service.

---
+++ blackcompany/concurrent.py
@@ -0,0 +1,6 @@
+""" Import this module first line in the file to have concurrent web service.
+	import blackcompany.concurrent
+Defaul WSGI implementation used by Bottle does not allow concurrent service
+so incoming requests stuck in queue.
+"""
+from gevent import monkey; monkey.patch_all()
