a066b500c612dc7bf98206f594c0ffdd7b7556a3
Dixie Flatline <umi0451@gmail.com>
2021-12-19 14:09:30 +0200

    Fixed CLI runner.

--- blackcompany/_base.py
+++ blackcompany/_base.py
@@ -3,12 +3,15 @@ import bottle
 def run(host='0.0.0.0', port=8080, debug=False, server_class=None):
 	""" Runs web server on given host/port.
 	"""
+	kwargs = {}
+	if server_class: # pragma: no cover
+		kwargs['server'] = server_class
 	while True:
 		try:
 			return bottle.run(host=host, port=port,
 				quiet=not debug, debug=debug,
 				reloader=False,
-				server=server_class,
+				**kwargs
 				)
 		except FileNotFoundError: # pragma: no cover -- Windows sometimes may interrupt bottle internal loop.
 			continue
@@ -27,4 +30,4 @@ def run_cli(host='0.0.0.0', port=8080): # pragma: no cover -- wrapper
 	@click.option('--debug', is_flag=True, help='Shows debug output.')
 	def cli(debug=False):
 		run(host=host, port=port, debug=debug)
-	return cli
+	return cli()
