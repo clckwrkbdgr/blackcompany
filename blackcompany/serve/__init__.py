1ee639bf160b34d1ad1519bdd6f85e859e03f59c
Dixie Flatline <umi0451@gmail.com>
2022-01-18 19:46:28 +0200

    Restoring import for backward compatibility.

--- blackcompany/serve/__init__.py
+++ blackcompany/serve/__init__.py
@@ -2,4 +2,5 @@ from ._base import *
 from .mime import *
 from . import rest
 from . import directory, text # deprecated import: For backward compatibility only.
+from .directory import static_content # deprecated import: For backward compatibility only.
 RemoteInfo = netutil.RemoteInfo # deprecated import: For backward compatibility only.
